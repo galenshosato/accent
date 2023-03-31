62f2f79ec047236070454ad6c01e7ac193761fca
Galen Sho Sato <galen.sato@gmail.com>
2023-03-31 12:43:33 -0400

    First commit

---
+++ server/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request, make_response
+from extensions import *
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+app.secret_key = 'woo secret key'
+
+db.init_app(app)
+migrate.init_app(app, db)
+
+
+@app.route('/')
+def home():
+    return ''
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
